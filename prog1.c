db32c2601ade572faa510e6ee0d468f8c41088a5
Sibasishnandy <142724809+Sibasishnandy@users.noreply.github.com>
2023-08-24 19:41:39 +0530

    Update prog1.c

--- prog1.c
+++ prog1.c
@@ -1,2 +1,7 @@
 //This is a test program. Contributors can write their code here.
 //Make all your changes in this program only.
+#include<stdio.h>
+int main()
+{
+  printf("Fuck u BItch!!!!!!");//Edited by sibasish
+}
